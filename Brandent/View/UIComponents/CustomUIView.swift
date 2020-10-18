8765edef1b603e74a7df0e03dc50382756e18ce7
Sara Babaei <sarababaei212@gmail.com>
2020-10-18 15:29:49 +0330

    TaskManager UI Started

--- Brandent/View/UIComponents/CustomUIView.swift
+++ Brandent/View/UIComponents/CustomUIView.swift
@@ -13,8 +13,57 @@ import UIKit
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             self.layer.cornerRadius = self.cornerRadius
+            
+            if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer {
+                topLayer.cornerRadius = self.cornerRadius
+            }
         }
     }
+    
+    @IBInspectable var lightGradientColor: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var darkGradientColor: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var vertical: Bool = true {
+        didSet {
+            updateGradientDirection()
+        }
+    }
+    
+    private func setGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+        gradientLayer.cornerRadius = self.layer.cornerRadius
+        gradientLayer.colors = [lightGradientColor.cgColor, darkGradientColor.cgColor]
+        
+        removeGradient()
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func removeGradient() {
+        if let gradientLayer = self.getGradientLayer() {
+            gradientLayer.removeFromSuperlayer()
+        }
+    }
+    
+    private func getGradientLayer() -> CALayer? {
+        if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer {
+            return topLayer
+        }
+        return nil
+    }
+    
+//    func updateGradientDirection() {
+//        gradientLayer.endPoint = vertical ? CGPoint(x: 0, y: 1) : CGPoint(x: 1, y: 0)
+//    }
 }
 
 extension UIView {
